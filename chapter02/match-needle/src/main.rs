136045100c93457c31a12b59baae3685458a9ecb
LuisGilGB <luis.gilgb@gmail.com>
2022-06-14 23:51:43 +0200

    Match needle example

---
+++ chapter02/match-needle/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let _needle = 42;
+    let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
+
+    let hit = "hit!";
+    let miss = "miss!";
+
+    for item in &haystack {
+        let result = match item {
+            42 | 132 => hit,
+            _ => miss,
+        };
+
+        if result == hit {
+            println!("{}, {}", item, result);
+        } else {
+            println!("{} missed", item);
+        }
+    }
+}

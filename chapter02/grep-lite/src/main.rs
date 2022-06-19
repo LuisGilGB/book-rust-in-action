b1c852780a1249c31d994e0cfef778dec6920dba
LuisGilGB <luis.gilgb@gmail.com>
2022-06-19 15:23:44 +0200

    Added stadin support to grep-lite

--- chapter02/grep-lite/src/main.rs
+++ chapter02/grep-lite/src/main.rs
@@ -1,32 +1,11 @@
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 use regex::Regex;
 use clap::{App, Arg};
 
-fn main() {
-    let args = App::new("grep-lite")
-        .version("0.1")
-        .about("searches for patterns")
-        .arg(Arg::with_name("pattern")
-            .help("The pattern to search for")
-            .takes_value(true)
-            .required(true)
-        )
-        .arg(Arg::with_name("input")
-            .help("File to search")
-            .takes_value(true)
-            .required(true)
-        )
-        .get_matches();
-
-    let pattern = args.value_of("pattern").unwrap();
-    let input = args.value_of("input").unwrap();
-
-    let re = Regex::new(pattern).unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
-
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
     let mut lines: Vec<String> = vec![];
 
     for line_ in reader.lines() {
@@ -73,4 +52,37 @@ fn main() {
         }
         println!("---------");
     }
+}
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern")
+            .help("The pattern to search for")
+            .takes_value(true)
+            .required(true)
+        )
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(true)
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let input = args.value_of("input").unwrap_or("-");
+
+    if input == "-" {
+        let std_in = io::stdin();
+        let reader = std_in.lock();
+        process_lines(reader, re)
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    }
+
 }

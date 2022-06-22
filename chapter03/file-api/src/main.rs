4e7e214e78bcc537b8a006386eae70445acab19c
LuisGilGB <luis.gilgb@gmail.com>
2022-06-23 00:39:33 +0200

    Constructor and read methods for File struct

--- chapter03/file-api/src/main.rs
+++ chapter03/file-api/src/main.rs
@@ -2,41 +2,57 @@
 
 #[derive(Debug)]
 struct File {
-    name: String,
-    data: Vec<u8>,
+  name: String,
+  data: Vec<u8>,
 }
 
-fn open(f: &mut File) -> bool {
-    true
-}
+impl File {
+  fn new(name: &str) -> File {
+    File {
+      name: String::from(name),
+      data: Vec::new(),
+    }
+  }
 
-fn close(f: &mut File) -> bool {
-    true
-}
+  fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+    let mut file = File::new(name);
+    file.data = data.clone();
+    file
+  }
 
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
+  fn read(
+    self: &File,
+    save_to: &mut Vec<u8>
+  ) -> usize {
+    let mut tmp = self.data.clone();
     let read_length = tmp.len();
     save_to.reserve(read_length);
     save_to.append(&mut tmp);
     read_length
+  }
 }
 
+fn open(f: &mut File) -> bool {
+  true
+}
+
+fn close(f: &mut File) -> bool {
+  true
+}
+
+
 fn main() {
-    let mut f1 = File {
-        name: String::from("f1.txt"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+  let mut f1 = File::new_with_data("f1.txt", &vec![114, 117, 115, 116, 33, 33]);
 
-    let mut buffer: Vec<u8> = vec![];
+  let mut buffer: Vec<u8> = vec![];
 
-    open(&mut f1);
-    let f1_length = read(&mut f1, &mut buffer);
-    close(&mut f1);
+  open(&mut f1);
+  let f1_length = f1.read(&mut buffer);
+  close(&mut f1);
 
-    let text = String::from_utf8_lossy(&buffer);
+  let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f1);
-    println!("{} is {} bytes long", &f1.name, &f1_length);
-    println!("{}", text);
+  println!("{:?}", f1);
+  println!("{} is {} bytes long", &f1.name, &f1_length);
+  println!("{}", text);
 }

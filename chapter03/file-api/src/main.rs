37c4b43cd0878b3b50749e7428de0341cbecf5e1
LuisGilGB <luis.gilgb@gmail.com>
2022-06-23 00:17:31 +0200

    Read function implemented

--- chapter03/file-api/src/main.rs
+++ chapter03/file-api/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(unused_variables)]
+
 #[derive(Debug)]
 struct File {
     name: String,
@@ -13,23 +14,29 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
 
 fn main() {
     let mut f1 = File {
         name: String::from("f1.txt"),
-        data: Vec::new(),
+        data: vec![114, 117, 115, 116, 33],
     };
 
-    let f1_name = &f1.name;
-    let f1_length = &f1.data.len();
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f1);
+    let f1_length = read(&mut f1, &mut buffer);
+    close(&mut f1);
+
+    let text = String::from_utf8_lossy(&buffer);
 
     println!("{:?}", f1);
-    println!("{} is {} bytes long", f1_name, f1_length);
-    open(&mut f1);
-    //read(&mut f1, &mut vec![]);
-    close(&mut f1);
+    println!("{} is {} bytes long", &f1.name, &f1_length);
+    println!("{}", text);
 }

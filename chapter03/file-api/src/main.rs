fe95d12266f86531f49979a0791122e251f6d3f9
LuisGilGB <luis.gilgb@gmail.com>
2022-06-26 18:01:59 +0200

    Basic and unsafe error handling

--- chapter03/file-api/src/main.rs
+++ chapter03/file-api/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_variables)]
 
+static mut ERROR: i32 = 0;
+
 #[derive(Debug)]
 struct File {
   name: String,
@@ -37,22 +39,33 @@ fn open(f: &mut File) -> bool {
 }
 
 fn close(f: &mut File) -> bool {
+  unsafe { ERROR = -1; }
   true
 }
 
 
 fn main() {
-  let mut f1 = File::new_with_data("f1.txt", &vec![114, 117, 115, 116, 33, 33]);
+  let mut file = File::new_with_data("f1.txt", &vec![114, 117, 115, 116, 33, 33]);
 
   let mut buffer: Vec<u8> = vec![];
 
-  open(&mut f1);
-  let f1_length = f1.read(&mut buffer);
-  close(&mut f1);
+  open(&mut file);
+  let f1_length = file.read(&mut buffer);
+  unsafe {
+    if ERROR != 0 {
+      panic!("An error has occurred while reading the file")
+    }
+  }
+  close(&mut file);
+  unsafe {
+    if ERROR != 0 {
+      panic!("An error has occurred while closing the file")
+    }
+  }
 
   let text = String::from_utf8_lossy(&buffer);
 
-  println!("{:?}", f1);
-  println!("{} is {} bytes long", &f1.name, &f1_length);
+  println!("{:?}", file);
+  println!("{} is {} bytes long", &file.name, &f1_length);
   println!("{}", text);
 }
